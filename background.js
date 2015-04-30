5a51c970125ad888aa0c3f06608d74f2585fc3f0
Renato Gama <renatoargh@gmail.com>
2015-04-30 20:26:40 -0300

    Implementado histórico de pesagens

--- background.js
+++ background.js
@@ -1,6 +1,11 @@
+function formatarPlaca(texto) {
+    texto = texto.trim().replace(/-/g, "");
+    return texto.substr(0, 3).toUpperCase() + "-" + texto.substr(3, 4);
+}
+
 chrome.contextMenus.create({
     id: 'raiz',
-    title: 'γBalança',
+    title: 'Balança Rodoviária',
     contexts: [
         'page',
         'editable'
@@ -15,19 +20,55 @@ chrome.contextMenus.create({
         'page'
     ],
     onclick: function(info, tab) {
-        chrome.tabs.sendRequest(tab.id, 'obterPeso');
+        chrome.tabs.sendRequest(tab.id, {
+            tipo: 'registrarEntrada'
+        });
     }
 });
 
-// chrome.contextMenus.create({
-//     id: 'registrarSaida',
-//     parentId: 'raiz',
-//     title: 'JHK-2343 (5.410 Kg)',
-//     contexts: [
-//         'editable'
-//     ],
-//     onclick: function(info, tab) {
-//         chrome.tabs.sendRequest(tab.id, 'Hello World!');
-//     }
-// });
+chrome.contextMenus.create({
+    id: 'historicoDePesagens',
+    parentId: 'raiz',
+    title: 'Exibir Histórico',
+    contexts: [
+        'page'
+    ],
+    onclick: function(info, tab) {
+        chrome.tabs.sendRequest(tab.id, {
+            tipo: 'exibirHistoricoDePesagens'
+        });
+    }
+});
+
+var acoes = {};
+
+acoes.ticketInserido = function adicionarNovoMenuDeContexto(request, sender, sendResponse) {
+    var ticket = request.dados;
+
+    chrome.contextMenus.create({
+        id: 'ticket' + ticket.id,
+        parentId: 'raiz',
+        title: formatarPlaca(ticket.placa) + ' (' + ticket.pesoInicial + ' Kg)',
+        contexts: [
+            'editable',
+            'page' // TODO: Excluir depois
+        ],
+        onclick: function(info, tab) {
+            chrome.tabs.sendRequest(tab.id, {
+                tipo: 'registrarSaida',
+                dados: ticket
+            });
+        }
+    });
+}
+
+acoes.ticketFinalizado = function(request, sender, sendResponse) {
+    var ticketId = request.dados;
+    chrome.contextMenus.remove('ticket' + ticketId);
+}
+
+chrome.runtime.onMessage.addListener(function(acao, sender, sendResponse) {
+    var fn = acoes[acao.tipo];
+    fn(acao, sender, sendResponse);
+});
 
--- content_script.js
+++ content_script.js
@@ -2,54 +2,209 @@ $.get(chrome.extension.getURL('html/obterPesoModal.html'), function(data) {
     $(data).appendTo('body');
 });
 
-var clickedEl = null,
-    baseUrl = '192.168.1.100',
-    deveCarregarPeso = false,
-    requests = {};
+$.get(chrome.extension.getURL('html/historicoDePesagens.html'), function(data) {
+    $(data).appendTo('body');
+});
 
-function carregarPeso(callback) {
-    $.getJSON(baseUrl + '/peso').complete(callback);
+function formatarPlaca(texto) {
+    texto = texto.trim().replace(/-/g, "");
+    return texto.substr(0, 3).toUpperCase() + "-" + texto.substr(3, 4);
 }
 
-requests.obterPeso = function() {
-    var $modal = $('#obterPesoModal');
+var elementoSelecionado = null,
+    baseUrl = 'http://127.0.0.1:7070',
+    deveCarregarPeso = false,
+    acoes = {};
 
-    // Inserir aqui um loop para buscar o peso a cada 333ms OU melhor ainda, usar socket.io
-    // para receber os novos pesos por push
-    // $modal.find('h1#peso').html(peso.valorFormatado);
+acoes.registrarEntrada = function() {
+    var $modal = $('#obterPesoModal'),
+        $placa = $modal.find('#placa');
 
-    $modal.on('show', function() {
-        // $modal.find('button#cancelarPeso').on('click', function(e) {
-        //     $modal.modal('hide');
-        // });
+    $placa.val('');
+    $placa.prop('readonly', false);
+    $modal.find('input#pesoInicial').val('');
+    $modal.find('.row.pesoInicial').addClass('hidden');
 
-        $modal.find('button#registrarPeso').on('click', function(e) {
+    var poolId = setInterval(function() {
+        var get = $.getJSON(baseUrl + '/peso');
+
+        get.done(function(peso) {
+            if(peso.valor === null) {
+                return;
+            }
+
+            $modal.find('h1 > span#pesoAtual').html(peso.valor + 'Kg');
+            $modal.find('h3#statusAtual').html({
+                estavel: 'ESTÁVEL',
+                instavel: 'INSTAVEL'
+            }[peso.status])
+                .removeClass('text-success')
+                .removeClass('text-danger')
+                .addClass({
+                    estavel: 'text-success',
+                    instavel: 'text-danger'
+                }[peso.status]);
+        });
+    }, 333);
+
+    $modal.one('show.bs.modal', function() {
+        $modal.find('button#registrarPeso').one('click', function(e) {
             var post = $.ajax({
-                url: baseUrl + '/ticket',
+                url: baseUrl + '/tickets',
+                type: 'POST',
                 data: {
                     placa: $modal.find('#placa').val()
                 }
             });
 
-            post.done(function() {
-                alert('Peso registrado com sucesso!');
+            post.done(function(ticket) {
+                chrome.runtime.sendMessage({
+                    tipo: 'ticketInserido',
+                    dados: ticket
+                });
+
                 $modal.modal('hide');
+                alert('Peso registrado com sucesso!');
+            });
+
+            post.fail(function() {
+                alert('Houve um erro registrando o peso!');
             });
         });
     });
 
-    $modal.on('hide', function() {
+    $modal.one('hide.bs.modal', function() {
         $modal.find('button#cancelarPeso').off('click');
         $modal.find('button#registrarPeso').off('click');
     });
 
-    $modal.on('hidden', function() {
+    $modal.one('hidden.bs.modal', function() {
+        clearInterval(poolId);
+
+        $modal.find('h1 > span#pesoAtual').html('');
+        $modal.find('#placa').val('');
+    });
+
+    $modal.modal({
+        'keyboard': true,
+        'show': true
+    });
+}
+
+acoes.registrarSaida = function registrarSaida(ticket) {
+    var $modal = $('#obterPesoModal'),
+        $placa = $modal.find('#placa');
+
+    $modal.find('input#pesoInicial').val(ticket.pesoInicial + 'Kg');
+    $modal.find('.row.pesoInicial').removeClass('hidden');
+    $placa.val(formatarPlaca(ticket.placa));
+    $placa.prop('readonly', true);
+
+    var poolId = setInterval(function() {
+        var get = $.getJSON(baseUrl + '/peso');
+
+        get.done(function(peso) {
+            if(!peso.valor) {
+                return;
+            }
+
+            $modal.find('h1 > span#pesoAtual').html(peso.valor + 'Kg');
+        });
+    }, 333);
+
+    $modal.one('show.bs.modal', function() {
+        $modal.find('button#registrarPeso').one('click', function(e) {
+            var put = $.ajax({
+                url: baseUrl + '/tickets/' + ticket.id,
+                type: 'PUT'
+            });
+
+            put.done(function(ticket) {
+                if(elementoSelecionado) {
+                    $(elementoSelecionado).val(ticket.pesoLiquido);
+                }
+
+                window.open(baseUrl + '/tickets/' + ticket.id + '/pdf');
+
+                chrome.runtime.sendMessage({
+                    tipo: 'ticketFinalizado',
+                    dados: ticket.id
+                });
+
+                $modal.modal('hide');
+                alert('Peso registrado com sucesso!');
+            });
+
+            put.fail(function() {
+                $modal.modal('hide');
+                alert('Houve um erro registrando o peso!');
+            });
+        });
+    });
+
+    $modal.one('hide.bs.modal', function() {
+        $modal.find('button#cancelarPeso').off('click');
+        $modal.find('button#registrarPeso').off('click');
+    });
+
+    $modal.one('hidden.bs.modal', function() {
+        clearInterval(poolId);
+
         $modal.find('h1#peso').html('');
         $modal.find('#placa').val('');
     });
 
     $modal.modal({
-        'backdrop': 'static',
+        'keyboard': true,
+        'show': true
+    });
+}
+
+acoes.exibirHistoricoDePesagens = function() {
+    var $modal = $('#historicoDePesagensModal'),
+        $tbody = $modal.find('table#historicoDePesagens tbody');
+
+    $tbody.empty();
+    $modal.one('show.bs.modal', function() {
+        var get = $.getJSON(baseUrl + '/tickets');
+
+        get.done(function(tickets) {
+            tickets.forEach(function(ticket) {
+                var $tr = $('<tr />');
+                $tr.append($('<td />').html(ticket.id));
+                $tr.append($('<td />').html(formatarPlaca(ticket.placa)));
+                $tr.append($('<td />').html(moment(ticket.dataDeEntrada).format('DD/MM/YYYY HH:mm')));
+                $tr.append($('<td />').html(ticket.pesoInicial + 'Kg'));
+                $tr.append($('<td />').html(ticket.dataDeSaida && moment(ticket.dataDeSaida).format('DD/MM/YYYY HH:mm')));
+                $tr.append($('<td />').html(ticket.pesoFinal && ticket.pesoFinal + 'Kg'));
+                $tr.append($('<td />').append($('<strong />').html(ticket.pesoFinal && (ticket.pesoFinal - ticket.pesoInicial) + 'Kg')));
+
+                if(ticket.pesoFinal) {
+                    $tr.append($('<td />').append($('<a href="' + baseUrl + '/tickets/' + ticket.id + '/pdf" target="_blank">EXIBIR TICKET</a>')));
+                } else {
+                    var $registrarSaida = $('<a href="#">REGISTRAR SAÍDA</a>');
+                    $registrarSaida.bind('click', function(e) {
+                        e.preventDefault();
+
+                        $modal.modal('hide');
+                        $modal.one('hidden.bs.modal', function() {
+                            acoes.registrarSaida(ticket);
+                        });
+                    });
+
+                    $tr.append($('<td />').append($registrarSaida));
+                }
+
+                $tbody.append($tr);
+            });
+        });
+
+        get.fail(function() {
+            alert('Houve um erro buscando o histórico de pesagens!');
+        });
+    });
+
+    $modal.modal({
         'keyboard': true,
         'show': true
     });
@@ -60,12 +215,10 @@ document.addEventListener("mousedown", function(event) {
         return;
     }
 
-    clickedEl = event.target;
+    elementoSelecionado = event.target;
 }, true);
 
-chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
-    console.log(arguments);
-
-    var fn = requests[request];
-    fn();
+chrome.extension.onRequest.addListener(function(acao, sender, sendResponse) {
+    var fn = acoes[acao.tipo];
+    fn(acao.dados);
 });
